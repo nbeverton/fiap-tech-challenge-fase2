9b8771d78917d2bdc71581cf8c54ed7baff8541c
novaes1998 <lucas.novaes1109@gmail.com>
2025-12-09 22:41:52 -0300

    feat: add UserType enum, User domain model, UserDocument and AppConfig

---
+++ src/main/java/br/com/fiap/techchallenge/G13/TechChallenge2/G13/infra/persistence/documents/UserDocument.java
@@ -0,0 +1,93 @@
+package br.com.fiap.techchallenge.G13.TechChallenge2.G13.infra.persistence.documents;
+
+import br.com.fiap.techchallenge.G13.TechChallenge2.G13.core.domain.enums.UserType;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document("user")
+public class UserDocument {
+
+    @Id
+    private String id;
+    private String name;
+    private UserType type;
+    private String email;
+    private String login;
+    private String password;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedBy
+    private LocalDateTime updateAt;
+
+
+    public UserDocument() { }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+}
