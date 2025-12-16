19c9d81b6c829ad3449833b00e6a4b85dfad5699
novaes1998 <lucas.novaes1109@gmail.com>
2025-12-16 18:09:24 -0300

    feat: add create user request dto

--- src/main/java/br/com/fiap/techchallenge/G13/TechChallenge2/G13/infra/web/dto/user/CreateUserRequest.java
+++ src/main/java/br/com/fiap/techchallenge/G13/TechChallenge2/G13/infra/web/dto/user/CreateUserRequest.java
@@ -8,4 +8,29 @@ public record CreateUserRequest (
         String email,
         String login,
         String password
-){}
+){
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public UserType type() {
+        return type;
+    }
+
+    @Override
+    public String email() {
+        return email;
+    }
+
+    @Override
+    public String login() {
+        return login;
+    }
+
+    @Override
+    public String password() {
+        return password;
+    }
+}
