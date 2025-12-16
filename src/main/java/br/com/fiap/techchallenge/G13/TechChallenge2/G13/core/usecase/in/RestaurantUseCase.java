10ec8ee128268fe7a39d3ad23013d517413c2fff
novaes1998 <lucas.novaes1109@gmail.com>
2025-12-16 11:33:56 -0300

    refactor: remove classes to avoid conflicts during project redesign

--- src/main/java/br/com/fiap/techchallenge/G13/TechChallenge2/G13/core/usecase/in/RestaurantUseCase.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.techchallenge.G13.TechChallenge2.G13.core.usecase.in;
-
-public interface RestaurantUseCase {
-    Restaurant create(Restaurant restaurant);
-    Restaurant update(Long id, Restaurant restaurant);
-    void delete(Long id);
-    Restaurant findById(Long id);
-    List<Restaurant> findAll();
-}
--- src/main/java/br/com/fiap/techchallenge/G13/TechChallenge2/G13/core/usecase/out/RestaurantRepositoryPort.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.techchallenge.G13.TechChallenge2.G13.core.usecase.out;
-
-public interface RestaurantRepositoryPort {
-    Restaurant save(Restaurant restaurant);
-    Optional<Restaurant> findById(Long id);
-    List<Restaurant> findAll();
-    void delete(Long id);
-}
