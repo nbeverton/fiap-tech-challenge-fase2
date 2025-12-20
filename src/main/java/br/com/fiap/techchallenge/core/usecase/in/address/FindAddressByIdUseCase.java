e3a1b56d88a371abbb4f54051cd2d0e17273628b
novaes1998 <lucas.novaes1109@gmail.com>
2025-12-20 18:44:39 -0300

    refactor(address): align findById use case with Optional return

--- src/main/java/br/com/fiap/techchallenge/core/usecase/in/address/FindAddressByIdUseCase.java
+++ src/main/java/br/com/fiap/techchallenge/core/usecase/in/address/FindAddressByIdUseCase.java
@@ -2,8 +2,10 @@ package br.com.fiap.techchallenge.core.usecase.in.address;
 
 import br.com.fiap.techchallenge.core.domain.model.Address;
 
+import java.util.Optional;
+
 public interface FindAddressByIdUseCase {
 
-    Address execute(String id);
+    Optional<Address> execute(String id);
 
 }
