d7dacee323d82b977298b210968e84572a76dee3
novaes1998 <lucas.novaes1109@gmail.com>
2025-12-23 17:18:23 -0300

    feat(config): add UserAddressBeanConfig

---
+++ src/main/java/br/com/fiap/techchallenge/infra/config/UserAddressBeanConfig.java
@@ -0,0 +1,45 @@
+package br.com.fiap.techchallenge.infra.config;
+
+import br.com.fiap.techchallenge.core.usecase.impl.useraddress.*;
+import br.com.fiap.techchallenge.core.usecase.in.useraddress.*;
+import br.com.fiap.techchallenge.core.usecase.out.UserAddressRepositoryPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserAddressBeanConfig {
+
+    @Bean
+    public CreateUserAddressUseCase createUserAddressUseCase(UserAddressRepositoryPort repository){
+
+        return new CreateUserAddressUseCaseImpl(repository);
+    }
+
+
+    @Bean
+    public DeleteUserAddressUseCase deleteUserAddressUseCase(UserAddressRepositoryPort repository){
+
+        return new DeleteUserAddressUseCaseImpl(repository);
+    }
+
+
+    @Bean
+    public FindUserAddressByAddressIdUseCase findUserAddressByAddressIdUseCase(UserAddressRepositoryPort repository){
+
+        return new FindUserAddressByAddressIdUseCaseImpl(repository);
+    }
+
+
+    @Bean
+    public FindUserAddressByUserIdUseCase findUserAddressByUserIdUseCase(UserAddressRepositoryPort repository){
+
+        return new FindUserAddressByUserIdUseCaseImpl(repository);
+    }
+
+
+    @Bean
+    public UpdateUserAddressUseCase updateUserAddressUseCase(UserAddressRepositoryPort repository){
+
+        return new UpdateUserAddressUseCaseImpl(repository);
+    }
+}
