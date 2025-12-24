ab915d485c45ac8f667948d37bf7ef0832234ddc
novaes1998 <lucas.novaes1109@gmail.com>
2025-12-24 18:22:58 -0300

    refactor: decouple UserResponse from domain UserType enum

--- src/main/java/br/com/fiap/techchallenge/infra/web/dto/user/UserResponse.java
+++ src/main/java/br/com/fiap/techchallenge/infra/web/dto/user/UserResponse.java
@@ -1,11 +1,9 @@
 package br.com.fiap.techchallenge.infra.web.dto.user;
 
-import br.com.fiap.techchallenge.core.domain.enums.UserType;
-
 public record UserResponse(
         String id,
         String name,
-        UserType type,
+        String type,
         String email,
         String login
 ) {
