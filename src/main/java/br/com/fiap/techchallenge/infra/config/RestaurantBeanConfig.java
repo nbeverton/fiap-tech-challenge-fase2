278e8d80dcbf78ded3a08ff98f85ab99c70b1bca
novaes1998 <lucas.novaes1109@gmail.com>
2026-01-07 23:06:19 -0300

    refactor(config): rename RestaurantBeanConfig

--- src/main/java/br/com/fiap/techchallenge/infra/config/RestaurantBeanConfig.java
+++ src/main/java/br/com/fiap/techchallenge/infra/config/RestaurantBeanConfig.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BeanConfig {
+public class RestaurantBeanConfig {
 
     // ===================== Restaurant UseCases =====================
 
