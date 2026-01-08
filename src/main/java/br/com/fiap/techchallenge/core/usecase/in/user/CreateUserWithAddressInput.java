8734148b4e0a2f4e9d3117341db8de7fb4d85bad
novaes1998 <lucas.novaes1109@gmail.com>
2026-01-07 22:44:08 -0300

    feat(user): implement CreateUserWithAddress flow

---
+++ src/main/java/br/com/fiap/techchallenge/core/usecase/in/user/CreateUserWithAddressInput.java
@@ -0,0 +1,16 @@
+package br.com.fiap.techchallenge.core.usecase.in.user;
+
+import br.com.fiap.techchallenge.core.domain.enums.AddressType;
+import br.com.fiap.techchallenge.core.domain.enums.UserType;
+
+public record CreateUserWithAddressInput(
+        String name,
+        UserType userType,
+        String email,
+        String login,
+        String password,
+        String addressId,
+        AddressType addressType,
+        String label
+) {
+}
