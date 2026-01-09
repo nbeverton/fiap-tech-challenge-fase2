b582ab42c96e6cf20c234da4ffc1b5d30ddb596b
novaes1998 <lucas.novaes1109@gmail.com>
2026-01-08 21:53:15 -0300

    feat(core): add UserAddressSummaryOutput for user addresses query

---
+++ src/main/java/br/com/fiap/techchallenge/core/usecase/out/UserAddressSummaryOutput.java
@@ -0,0 +1,15 @@
+package br.com.fiap.techchallenge.core.usecase.out;
+
+import br.com.fiap.techchallenge.core.domain.enums.AddressType;
+
+public record UserAddressSummaryOutput(
+        AddressType addressType,
+        String streetName,
+        int streetNumber,
+        String neighborhood,
+        String city,
+        String stateProvince,
+        String additionalInfo,
+        boolean principal         
+) {
+}
