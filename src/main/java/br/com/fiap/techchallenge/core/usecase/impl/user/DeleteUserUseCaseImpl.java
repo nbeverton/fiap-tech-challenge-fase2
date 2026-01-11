74bf9df0b21edb5c7da43b8ca0a5ae85014aece6
novaes1998 <lucas.novaes1109@gmail.com>
2026-01-11 13:10:25 -0300

    refactor(core): delete user with cleanup of user address relations

--- src/main/java/br/com/fiap/techchallenge/core/usecase/impl/user/DeleteUserUseCaseImpl.java
+++ src/main/java/br/com/fiap/techchallenge/core/usecase/impl/user/DeleteUserUseCaseImpl.java
@@ -1,22 +1,40 @@
 package br.com.fiap.techchallenge.core.usecase.impl.user;
 
 import br.com.fiap.techchallenge.core.domain.exception.user.UserNotFoundException;
+import br.com.fiap.techchallenge.core.domain.model.UserAddress;
 import br.com.fiap.techchallenge.core.usecase.in.user.DeleteUserUseCase;
+import br.com.fiap.techchallenge.core.usecase.out.UserAddressRepositoryPort;
 import br.com.fiap.techchallenge.core.usecase.out.UserRepositoryPort;
+
+import java.util.List;
 
 public class DeleteUserUseCaseImpl implements DeleteUserUseCase {
 
     private final UserRepositoryPort userRepository;
+    private final UserAddressRepositoryPort userAddressRepository;
 
-    public DeleteUserUseCaseImpl(UserRepositoryPort userRepository){
+    public DeleteUserUseCaseImpl(UserRepositoryPort userRepository, UserAddressRepositoryPort userAddressRepository){
         this.userRepository = userRepository;
+        this.userAddressRepository = userAddressRepository;
     }
 
     @Override
     public void execute(String id) {
+
+        //1. Check if the user exists
         userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
 
+        //2. Find links with address
+        List<UserAddress> links =
+                userAddressRepository.findByUserId(id);
+
+        //3. Remove links
+        links.forEach(link ->
+                userAddressRepository.deleteById(link.getId())
+        );
+
+        //4. Delete the user
         userRepository.deleteById(id);
     }
 }
